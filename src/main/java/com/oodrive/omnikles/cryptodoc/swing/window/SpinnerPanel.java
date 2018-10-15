812703334202677685850af07a4806d71fbb5b24
omarcou <o.marcou@oodrive.fr>
2018-10-15 17:07:07 +0200

    OKTDR-621 add text

--- src/main/java/com/oodrive/omnikles/cryptodoc/swing/window/SpinnerPanel.java
+++ src/main/java/com/oodrive/omnikles/cryptodoc/swing/window/SpinnerPanel.java
@@ -1,12 +1,16 @@
 package com.oodrive.omnikles.cryptodoc.swing.window;
 
+import com.oodrive.omnikles.cryptodoc.CryptoDoc;
+import com.oodrive.omnikles.cryptodoc.swing.component.template.SummaryTextTemplate;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class SpinnerPanel extends JPanel{
 
-    private ImageIcon spinnerIcon =  new ImageIcon(new ImageIcon(OpenReceivership.class.getResource("/load.gif")).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-
+    private ImageIcon spinnerIcon =  new ImageIcon(new ImageIcon(OpenReceivership.class.getResource("/load.gif"))
+            .getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT));
+    SummaryTextTemplate label = new SummaryTextTemplate(CryptoDoc.textProperties.getProperty("open.loading.paragraphe1"));
     public SpinnerPanel(){
         setOpaque(false);
         setFocusable(false);
@@ -14,7 +18,8 @@ public class SpinnerPanel extends JPanel{
         JLabel loadingIcon = new JLabel();
         loadingIcon.setOpaque(false);
         loadingIcon.setIcon(spinnerIcon);
-        add(loadingIcon,BorderLayout.CENTER);
+        add(loadingIcon, BorderLayout.CENTER);
+        add(label, BorderLayout.SOUTH);
         setVisible(false);
     }
 }
