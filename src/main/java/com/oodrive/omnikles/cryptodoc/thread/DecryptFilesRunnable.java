eeacf029b0f319ed38de0ca33885547155d9d1a2
omarcou <o.marcou@oodrive.fr>
2018-12-07 15:43:52 +0100

    OKMRCH-3406

--- src/main/java/com/oodrive/omnikles/cryptodoc/thread/DecryptFilesRunnable.java
+++ src/main/java/com/oodrive/omnikles/cryptodoc/thread/DecryptFilesRunnable.java
@@ -14,6 +14,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 import java.security.cert.CertificateEncodingException;
 import java.util.ArrayList;
@@ -39,37 +40,52 @@ public class DecryptFilesRunnable extends Thread{
         this.certificates = certificates;
     }
 
+    private int chooser(String msg){
+        return JOptionPane.showConfirmDialog(this.parent, msg,
+                CryptoDoc.textProperties.getProperty("message.warning.title"), JOptionPane.YES_NO_OPTION);
+    }
+
     @Override
     public void run() {
         errorOpener = null;
+        boolean isDecrypted = false;
         parent.getPage3Paragraphe2().setText(CryptoDoc.textProperties.getProperty("open.page3.decrypt"));
         for(int i =0 ; i< selectDeposit.size(); i++){
             if(selectDeposit.get(i).getCheck().isSelected()) {
                 Logs.sp("Selected zip :  " + selectDeposit.get(i).getFile().getName());
                 if(Configuration.isOkMarches){
-                    DecryptOkMarchesService dos = DecryptOkMarchesService.getInstance();
-                    Logs.sp("Debut decrypt plis ...");
 
-                    String compteRenduFile = dos.makeCr(dos.openEnveloppe(selectDeposit.get(i).getFile(), selectDeposit.get(i).getDepositStatus()));//a retourner lors de l'update du status du pli ouvert
-                    Logs.sp("Compte rendu ... " + compteRenduFile);
-                    Logs.sp("Fin decrypt plis ... ");
-                        DepositStatus depositStatus = selectDeposit.get(i).getDepositStatus();
-                    try {
-                        List<NameValuePair> params = new ArrayList<>();
-                        params.add(new BasicNameValuePair("idcand", "" + depositStatus.getId()));
-                        params.add(new BasicNameValuePair("crfile", compteRenduFile));
-                        params.add(new BasicNameValuePair("iddossier", Configuration.parameters.get("idDossier")));
-                        HashMap<String, Long> ids = Configuration.getIdsFile(selectDeposit.get(i).getFile().getName());
-                        String idlot = "0";
-                        if(ids.get("idlot") != null){
-                            idlot = ""+ids.get("idlot");
+                    DepositStatus depositStatus = selectDeposit.get(i).getDepositStatus();
+                    File destination = new File(selectDeposit.get(i).getFile().getParent() + File.separator + depositStatus.getNumLot() + File.separator + depositStatus.getSupplierName());
+                    int choose = -1;
+                    if(destination.exists() && destination.isDirectory())
+                        choose = chooser(CryptoDoc.textProperties.getProperty("message.chooser.folder.exist").replace("<depot>", depositStatus.getFilename()));
+                    if(choose != 1)
+                    {
+                        isDecrypted = true;
+                        DecryptOkMarchesService dos = DecryptOkMarchesService.getInstance();
+                        Logs.sp("Debut decrypt plis ...");
+
+                        String compteRenduFile = dos.makeCr(dos.openEnveloppe(selectDeposit.get(i).getFile(), depositStatus));//a retourner lors de l'update du status du pli ouvert
+                        Logs.sp("Compte rendu ... " + compteRenduFile);
+                        Logs.sp("Fin decrypt plis ... ");
+                        try {
+                            List<NameValuePair> params = new ArrayList<>();
+                            params.add(new BasicNameValuePair("idcand", "" + depositStatus.getId()));
+                            params.add(new BasicNameValuePair("crfile", compteRenduFile));
+                            params.add(new BasicNameValuePair("iddossier", Configuration.parameters.get("idDossier")));
+                            HashMap<String, Long> ids = Configuration.getIdsFile(selectDeposit.get(i).getFile().getName());
+                            String idlot = "0";
+                            if(ids.get("idlot") != null){
+                                idlot = ""+ids.get("idlot");
+                            }
+                            params.add(new BasicNameValuePair("idlot",  idlot));
+                            ssl.updateExchangeDocumentState(Configuration.parameters.get("urlUpdateStatus"), params);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }catch (NullPointerException e) {
+                            e.printStackTrace();
                         }
-                        params.add(new BasicNameValuePair("idlot",  idlot));
-                        ssl.updateExchangeDocumentState(Configuration.parameters.get("urlUpdateStatus"), params);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }catch (NullPointerException e) {
-                        e.printStackTrace();
                     }
                 }else{
                     KeyPair kp = null;
@@ -94,12 +110,17 @@ public class DecryptFilesRunnable extends Thread{
                 }
             }
         }
-        if(errorOpener != null){
-            error(CryptoDoc.textProperties.getProperty("message.error.open.text").replace("<error>", errorOpener) );
+        if(isDecrypted) {
+            if (errorOpener != null) {
+                error(CryptoDoc.textProperties.getProperty("message.error.open.text").replace("<error>", errorOpener));
+            } else {
+                parent.getPage3Paragraphe2().setText(CryptoDoc.textProperties.getProperty("open.page3.sending.result.ok"));
+            }
+            parent.getLoadingIcon().setVisible(false);
         }else{
-            parent.getPage3Paragraphe2().setText(CryptoDoc.textProperties.getProperty("open.page3.sending.result.ok"));
+            this.parent.setScreenNumber(2);
+            this.parent.activateScreen();
         }
-        parent.getLoadingIcon().setVisible(false);
         this.interrupt();
     }
 
